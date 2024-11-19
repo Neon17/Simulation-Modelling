e2b5d163eb023f2eda264e7ef1a49d4742efed1e
Neon17 <56569427+Neon17@users.noreply.github.com>
2024-11-19 21:03:05 +0545

    NEW: Monte Carlo Method of calculating PIE made

--- MonteCarloPie.cpp
+++ MonteCarloPie.cpp
@@ -2,19 +2,28 @@
 using namespace std;
 
 
-int calculatePie(int itrs){
+double calculatePie(long int itrs){
     //square points by circular points in an unit radius
-    int x,y,pi=0;
-    x = rand()%10;
-    cout<<"X = "<<x<<endl;
-    cout<<"y = "<<y<<endl;
+    double x,y,pi;
+    long int circlePoints=0, squarePoints=0;
+    srand(time(NULL));
+    for (long int i=0;i<itrs;i++){
+        x = (double)rand()/RAND_MAX;
+        y = (double)rand()/RAND_MAX;
+        if ((x*x + y*y)<1) circlePoints++;
+        squarePoints++;
+    }
+    cout<<"Number of points inside circle = "<<circlePoints<<endl;
+    cout<<"Number of points inside square = "<<squarePoints<<endl;
+    pi = 4*(double)circlePoints/squarePoints;
     return pi;
 }
 
 int main(){
-    int itrs;
+    cout<<"Monte Carlo: Finding the value of PI"<<endl;
+    long int itrs;
     cout<<"Enter number of iterations you want: ";
     cin>>itrs;
-    cout<<calculatePie(itrs)<<endl;
+    cout<<"PI = "<<calculatePie(itrs)<<endl;
     return 0;
 }
